7e7ea77a39df8858d2512e41652ce447bf6735a3
jerry <cyj1322@outlook.com>
2023-03-28 22:04:04 +0900

    initialize

---
+++ src/main/java/com/jerry/boilerplatespringboot/BoilerplateSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jerry.boilerplatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jerry/boilerplatespringboot/BoilerplateSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry.boilerplatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
