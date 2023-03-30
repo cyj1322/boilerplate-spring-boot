0230defb6bdef59b7592fe4f50959f6f10ed099f
jerry <cyj1322@outlook.com>
2023-03-30 18:23:27 +0900

    add jdbc repository

--- src/main/java/com/jerry/boilerplatespringboot/SpringConfig.java
+++ src/main/java/com/jerry/boilerplatespringboot/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.jerry.boilerplatespringboot;
 
+import com.jerry.boilerplatespringboot.repository.JdbcMemberRepository;
+import com.jerry.boilerplatespringboot.repository.JdbcTemplateMemberRepository;
 import com.jerry.boilerplatespringboot.repository.MemberRepository;
-import com.jerry.boilerplatespringboot.repository.MemoryMemberRepository;
 import com.jerry.boilerplatespringboot.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
